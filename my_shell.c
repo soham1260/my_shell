e2fe7487a5496596c445094854d6402b39392c50
soham1260 <114227865+soham1260@users.noreply.github.com>
2025-08-11 11:53:49 +0530

    added background execution

--- my_shell.c
+++ my_shell.c
@@ -8,6 +8,7 @@
 #define MAX_INPUT_SIZE 1024
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
+#define MAX_BG_PROCS 64
 
 char **tokenize(char *line)
 {
@@ -43,8 +44,29 @@ int main(int argc, char* argv[]) {
 	int i;
     char* cwd = (char*)malloc(256);
 
+    int bg_pids[MAX_BG_PROCS];
+    for (int i = 0; i < MAX_BG_PROCS; i++) {
+        bg_pids[i] = -1;
+    }
+
+    int bg_count = 0;
+
 	while(1) {			
         
+        for (int i = 0; i < MAX_BG_PROCS; i++) 
+        {
+            if (bg_pids[i] == -1) continue;
+            int status;
+            int res = waitpid(bg_pids[i], &status, WNOHANG);
+            if (res > 0) 
+            {
+                printf("\n[Background Done] PID %d exited with code %d\n", res, WEXITSTATUS(status));
+                bg_pids[i] = -1;
+                bg_count--;
+            }
+        }
+/////////////////////////////////////////////////////////////////////////////////
+ 
 		bzero(line, sizeof(line));
 
         if (getcwd(cwd,256) != NULL)
@@ -55,43 +77,95 @@ int main(int argc, char* argv[]) {
         {
             printf("<getcwd error>%s $ ", cwd);
         }
+        
+/////////////////////////////////////////////////////////////////////////////////
+
 		scanf("%[^\n]", line);
 		getchar();
 
 		line[strlen(line)] = '\n';
 		tokens = tokenize(line);
-        
+
+/////////////////////////////////////////////////////////////////////////////////
+
         if (tokens[0] != NULL && strcmp(tokens[0], "cd") == 0) 
         {
             if (tokens[1] == NULL || chdir(tokens[1]) != 0) 
             {
                 printf("cd error\n");
             }
+            for (int i = 0; tokens[i] != NULL; i++) 
+            {
+                free(tokens[i]);
+            }
+            free(tokens);
             continue;
+        }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+        int background = 0;
+        int last_token = 0;
+        while (tokens[last_token] != NULL) last_token++;
+
+        if (last_token > 0 && strcmp(tokens[last_token - 1], "&") == 0) 
+        {
+            background = 1;
+            free(tokens[last_token - 1]);
+            tokens[last_token - 1] = NULL;
+        }
+
+/////////////////////////////////////////////////////////////////////////////////
+
+        if (background && bg_count == MAX_BG_PROCS) 
+        {
+            printf("Too many background processes!\n");
+            background = 0;
+            for (int i = 0; tokens[i] != NULL; i++) 
+            {
+                free(tokens[i]);
+            }
+            free(tokens);
+            continue;
+        }
+        
+        int pid = fork();
+    
+        if(pid < 0)
+        {
+            printf("Error forking");
+        }
+        else if(pid == 0)
+        {
+            if(execvp(tokens[0],tokens) == -1)
+            {
+                printf("execvp error : Invalid Command\n");
+                exit(1);
+            }
         }
         else
         {
-            int pid = fork();
-
-            if(pid < 0)
-            {
-                printf("Error forking");
-            }
-            else if(pid == 0)
-            {
-                if(execvp(tokens[0],tokens) == -1)
-                {
-                    printf("execvp error : Invalid Command\n");
-                    exit(1);
-                }
-            }
-            else
+            if (!background) 
             {
                 int status;
-                wait(&status);
-                if (WIFEXITED(status))
+                waitpid(pid, &status, 0);
+                // if (WIFEXITED(status)) {
+                //     printf("EXITSTATUS: %d\n", WEXITSTATUS(status));
+                // }
+            } 
+            else 
+            {
+                printf("[Background] PID: %d\n", pid);
+                int i=0;
+                while(i<MAX_BG_PROCS)
                 {
-                    printf("EXITSTATUS: %d\n", WEXITSTATUS(status));
+                    if (bg_pids[i] == -1)
+                    {
+                        bg_pids[i] = pid;
+                        bg_count++;
+                        break;
+                    }
+                    i++;
                 }
             }
         }
